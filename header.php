0b26c9e6bca4e5f697caa7f6386d805840266711
sandordost <sandor1998@hotmail.com>
2021-12-13 23:57:12 +0100

    Implemented new header
    Oude header kon niet op gebouwd worden

--- header.php
+++ header.php
@@ -19,42 +19,51 @@ $databaseConnection = connectToDatabase();
     <link rel="stylesheet" href="Public/CSS/style.css" type="text/css">
     <link rel="stylesheet" href="Public/CSS/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="Public/CSS/typekit.css">
+    <link rel="stylesheet" href="Public/CSS/new-header.css">
 </head>
 <body>
-<div class="Background">
-    <div class="row" id="Header">
-        <div class="col-2"><a href="./" id="LogoA">
-                <div id="LogoImage"></div>
-            </a></div>
-        <div class="col-8" id="CategoriesBar">
-            <ul id="ul-class">
+    <div class="new-header">
+        <div class="left-bar">
+            <div class="logo">
+                <a href="./">
+                    <img src="Public/ProductIMGHighRes/NerdyGadgetsLogo.png" alt="">
+                </a>
+            </div>
+            <div class="categories">
                 <?php
                 $HeaderStockGroups = getHeaderStockGroups($databaseConnection);
 
                 foreach ($HeaderStockGroups as $HeaderStockGroup) {
                     ?>
-                    <li>
+                    <div class="category">
                         <a href="browse.php?category_id=<?php print $HeaderStockGroup['StockGroupID']; ?>"
                            class="HrefDecoration"><?php print $HeaderStockGroup['StockGroupName']; ?></a>
-                    </li>
+                    </div>
                     <?php
                 }
                 ?>
-                <li>
+                <div class="category">
                     <a href="categories.php" class="HrefDecoration">Alle categorieën</a>
-                </li>
-            </ul>
+                </div>
+            </div>
         </div>
-<!-- code voor US3: zoeken -->
-
-        <ul id="ul-class-navigation">
-            <li>
+        <div class="right-bar">
+            <div class="search-bar">
                 <a href="browse.php" class="HrefDecoration"><i class="fas fa-search search"></i> Zoeken</a>
-            </li>
-            <li>
-                <a href="cart.php" class="HrefDecoration"><i class="fas fa-shopping-cart search"></i> Winkelmand</a>
-            </li>
-        </ul>
+            </div>
+            <div id="account-container">
+                <div id="profile-info">
+                    <a id="account-naam" href="test.php"> Gast</a>
+                    <div id="shopping-cart-container">
+                        <a id="mijn-winkelmand" href="cart.php" id="winkelmand"><i class="fas fa-shopping-cart search"></i>Mijn winkelmand</a>
+                    </div>
+                </div>
+                <div id="profile-image">
+                    <i class="fas fa-user"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <!-- einde code voor US3 zoeken -->
     </div>
