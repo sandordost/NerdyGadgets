8c04209ca9b20a0d6be7bb41149e6195ccd9a038
Yonn Mulder <yonn.mulder@gmail.com>
2021-12-22 17:54:25 +0100

    Kleine fix 2

--- update-tables.php
+++ update-tables.php
@@ -5,7 +5,7 @@ function AddKortingToProduct($conn){
 
     //Update collumn only if not exists:
     try {
-        $sql = "ALTER TABLE stockitems ADD COLUMN korting DECIMAL(5,2) DEFAULT(0)";
+        $sql = "ALTER TABLE stockitems ADD COLUMN korting DECIMAL(5,2) DEFAULT 0";
 
         $stmt = $conn->prepare($sql);
         $stmt->execute();
